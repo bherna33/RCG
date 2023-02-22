c6142639543d992bad77415b4263c530ad6b08f6
Branden Henrnadez <Brandenjh98@gmail.com>
2023-02-21 23:34:24 -0500

    UI changes

--- request.py
+++ request.py
@@ -1,5 +1,5 @@
 from datetime import date
-from flask import Flask, request ,render_template, Flask, abort, flash
+from flask import Flask, request ,render_template, Flask, redirect
 import sqlite3
 
 # creates flask APP
@@ -22,45 +22,77 @@ def hello_world():
     return render_template("index.html", posts=post)
 
 # gets POST or GET responts 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['POST'])
 def verify_post(): 
     if request.method == "POST":
         insert(request.form['person_id'], request.form['first_name'], request.form['last_name'], request.form['email_address'], request.form['hire_date'], request.form['job_title'], "")
         return hello_world()
 
 # gets post using id
-def get_post(post_id):
+# def get_post(post_id):
+#     conn = get_db_connection()
+#     post = conn.execute('SELECT * FROM Employee WHERE person_id = ?', (post_id)).fetchone()
+#     conn.close()
+#     if post is  None:
+#         abort(404)
+#     return post
+
+
+@app.route('/<person_id>', methods=['POST'])
+def delete(person_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM Employee WHERE person_id = ?', (post_id)).fetchone()
+    conn.execute('DELETE FROM Employee WHERE person_id = ?', (person_id,))
+    conn.commit()
     conn.close()
-    if post is  None:
-        abort(404)
-    return post
+    # flash('"{}" was snapped away!'.format(post['first_name']))
+    return redirect("/")
+    
 
 
 # deletes employee by id number
-@app.route('/', methods=['POST'])
-def delete(person_id):
-    post = get_post(person_id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM Employee WHERE person_id = ?', (person_id))
-    conn.commit()
-    conn.close()
-    flash('"{}" was snapped away!'.format(post['first_name']))
-    # curser.execute(f"DELETE FROM Employee WHERE person_id = '{person_id}'; ")
-    return render_template("index.html")
+# @app.route('/<person_id>', methods=['GET'])
+# def delete(person_id):
+#     # return person_id
+#     # post = get_post(person_id)
+#     conn = get_db_connection()
+#     conn.execute('DELETE FROM Employee WHERE person_id = ?', (person_id,))
+#     conn.commit()
+#     conn.close()
+#     # flash('"{}" was snapped away!'.format(post['first_name']))
+#     return render_template("index.html")
  
 
-# updates employee by id 
-@app.route('/<int:id>/edit/', methods=['POST'])
-def edit(person_id, set_by):
-    post = get_post(person_id)
-    
-    # loop through dictinary to get column, value
-    for key, val in set_by.items():
-        # curser.execute(f"UPDATE Employee SET '{key}' = '{val}' WHERE person_id = '{person_id}';")
-        pass
+saved_id= None
 
+@app.route('/<person_id>', methods=['POST'])
+def save_id(person_id):
+    saved_id =person_id
+
+# updates employee by id 
+@app.route('/', methods=['POST'])
+def edit():
+    return render_template("index.html")
+    # stored_values= {
+    #     "first_name":request.form['first_name'], 
+    #     "last_name":request.form['last_name'], 
+    #     "email_address":request.form['email_address'], 
+    #     "hire_date":request.form['hire_date'], 
+    #     "job_title":request.form['job_title'], 
+    #     "agency_num":request.form['agency_num'], 
+    #     "registration_date":request.form['registration_date']
+    # }
+    # database = sqlite3.connect('test.db')
+    # curser = database.cursor()
+        
+    # for key, val in stored_values.items():
+    #     if not val:
+    #         curser.execute(f"UPDATE Employee SET {key} = {val} WHERE person_id = {saved_id};" ) 
+            
+    # database.commit()
+    # database.close()
+    # return redirect("/")
+          
+            
 
 # insert data to sql
 def insert(person_id, first_name, last_name, email_address, hire_date, job_title, agency_num):
